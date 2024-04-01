ef4c104339932c948223fae81dce0a73f8cb5ef9
Vladimir Khvorostov <vladimir@khvorostov.ru>
2024-04-01 11:14:36 +0000

    change db settings from sqlite to postgres

--- testshop/settings.py
+++ testshop/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'testshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testshop',
+        'USER': 'testshop',
+        'PASSWORD': "hello",
+        'HOST': "postgresdb",
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60,
     }
 }
 
